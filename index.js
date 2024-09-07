187c7fd2346e7b657e44b043ad76d0361b8acf03
Yogesh kumar <155542834+Yogesh283@users.noreply.github.com>
2024-09-07 06:59:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,5 @@ app.post('/submit', async (req, res) => {
 
 
 
-// Start the server on the specified port
-const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+
